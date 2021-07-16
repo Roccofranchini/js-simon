154f73cf714a3f6cb9f2484d082cb6b06a11dcee
Roccofranchini <rocco.franchini1@gmail.com>
2021-07-16 15:59:58 +0200

    punteggio-added

--- js/script.js
+++ js/script.js
@@ -21,9 +21,19 @@ indovinare sono stati individuati. (Bonus: stampare in pagina il risultato, in a
 var randomNumbers = [];
 var userChoices = [];
 
+var titleDisplay = document.getElementById('title-display');
+var display = document.getElementById('display');
+
+var titleDisplay2 = document.getElementById('title-display2');
+var display2 = document.getElementById('display2');
+
+var scoreDisplay = document.getElementById('score');
+
+
+
 while (randomNumbers.length < 5) {
     var randomNumber = getRandomNumber(1, 100);
-
+    
     if (!randomNumbers.includes(randomNumber)) {
         randomNumbers.push(randomNumber);
     }
@@ -36,19 +46,60 @@ alert(randomNumbers);
 // impostiamo 30 secondi di timer per chiedere i numeri all'utente
 
 setTimeout (function(){
-
+    
+    var score = 0;
     var i = 1;
-   while (userChoices.length < 5) {
-      var userChoice = prompt(i + '/5: digita un numero')
-       if (!userChoices.includes(userChoice)) {
-        userChoices.push(userChoice);
-        i++;
+
+    while (userChoices.length < 5) {
+        var userChoice = parseInt(prompt(i + '/5: digita un numero'));
+        if (!userChoices.includes(userChoice)) {
+            userChoices.push(userChoice);
+            i++;
+        }
+        
+        if (randomNumbers.includes(userChoice)) {
+            score = score + 1;
+            console.log(score);
     }
+
 }
 
 console.log(userChoices)
 
-}, 3000 );
+// grazie ad un for inseriamo i 5 numeri casuali in un UL -- posso fare una funzione
+
+var listElement = '';
+
+for (var i = 0; i < randomNumbers.length; i++) {
+
+    console.log(listElement);
+
+    listElement += '<li>' + randomNumbers[i] + '</li>';
+}
+
+display.innerHTML = listElement;
+
+// grazie ad un for inseriamo i 5 numeri dell'utente in un UL
+
+var urListElement = '';
+
+for (var i = 0; i < userChoices.length; i++) {
+
+    console.log(urListElement);
+
+    urListElement += '<li>' + userChoices[i] + '</li>';
+}
+
+display.innerHTML = listElement;
+display2.innerHTML = urListElement;
+titleDisplay.innerHTML = 'I numeri estratti sono: ';
+titleDisplay2.innerHTML = 'I tuoi numeri  sono: ';
+scoreDisplay.innerHTML = score;
+
+
+
+}, 30 );
+
 
 
 
